f1e04b59d538128b62db8e6b181b31bdbd8caa1b
macbook <Apple@Monsef.local>
2025-12-03 14:51:42 +0100

    create Accounts table

--- db.sql
+++ db.sql
@@ -5,8 +5,22 @@ CREATE TABLE Customers (
     phone VARCHAR(20),
     registration_date DATE
 );
+
 CREATE TABLE Advisors (
     advisor_id INT AUTO_INCREMENT PRIMARY KEY,
     full_name VARCHAR(100),
     email VARCHAR(100)
 );
+
+CREATE TABLE Accounts (
+    account_id INT AUTO_INCREMENT PRIMARY KEY,
+    account_number VARCHAR(50),
+    balance DECIMAL(12,2),
+    account_type ENUM('Checking','Savings','Business'),
+    customerid INT,
+    advisorid INT,
+    FOREIGN KEY (customerid) REFERENCES Customers(customer_id),
+    FOREIGN KEY (advisorid) REFERENCES Advisors(advisor_id)
+);
+
+
