9139d60b6513fe0db351bc6c99d356984186f3fc
macbook <Apple@Monsef.local>
2025-12-03 14:43:40 +0100

    create Customers table

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(100),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    registration_date DATE
+);
