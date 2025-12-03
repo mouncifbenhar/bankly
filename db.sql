89bc3cc74c0f233ec03a185f2ebee1e16482afe4
macbook <Apple@Monsef.local>
2025-12-03 14:47:54 +0100

    create Advisors table

--- db.sql
+++ db.sql
@@ -5,3 +5,8 @@ CREATE TABLE Customers (
     phone VARCHAR(20),
     registration_date DATE
 );
+CREATE TABLE Advisors (
+    advisor_id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(100),
+    email VARCHAR(100)
+);
