92a8ef713410447bc8970390cc06b27293db6b85
macbook <Apple@Monsef.local>
2025-12-03 16:00:20 +0100

    Edit a customer's phone number

--- query.sql
+++ query.sql
@@ -2,6 +2,8 @@ INSERT INTO Customers(full_name, email, phone, registration_date)
 VALUES
 ("redwan ridawi", "redwan@gmail.com", "0601010101",CURDATE());
 
+UPDATE Customers SET phone = "0607080901" WHERE customer_id = 1;
+
 
 
 
