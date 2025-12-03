e5a1271c2fac4d206a8a29aaae687f46bc807423
macbook <Apple@Monsef.local>
2025-12-03 16:16:34 +0100

    to task 23

--- query.sql
+++ query.sql
@@ -20,6 +20,44 @@ SELECT * FROM Accounts WHERE account_type = "Savings";
 
 SELECT * FROM Accounts WHERE customerid = 1 ;
 
+SELECT * FROM Accounts ORDER BY balance ASC;
+
+SELECT * FROM Transactions;
+
+SELECT * FROM Transactions WHERE transaction_type = "debit" ;
+
+SELECT * FROM Transactions WHERE transaction_type = "credit" ;
+
+SELECT * FROM Transactions WHERE accountid = 1 ;
+
+SELECT * FROM Transactions WHERE amount >= 500 ;
+
+SELECT * FROM Transactions WHERE amount BETWEEN 100 AND 1000;
+
+SELECT * FROM Transactions ORDER BY amount DESC;
+
+SELECT * FROM Transactions ORDER BY amount DESC LIMIT 2;
+
+SELECT * FROM Transactions ORDER BY transaction_date DESC;
+
+SELECT * FROM Transactions ORDER BY transaction_date DESC LIMIT 3 ;
+
+SELECT Customers.* FROM Customers JOIN Accounts ON customer_id = customerid WHERE advisorid = 2;
+
+SELECT
+  Accounts.account_id,
+  Accounts.account_number,
+  Accounts.balance,
+  Accounts.account_type,
+  Customers.full_name AS customer_name,
+  Advisors.full_name  AS advisor_name
+FROM Accounts
+JOIN Customers
+  ON Customers.customer_id = Accounts.customerid
+JOIN Advisors
+  ON Advisors.advisor_id = Accounts.advisorid;
+
+
 
 
 
